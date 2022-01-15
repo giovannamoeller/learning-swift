60223895369498c8f2926bcf309ba51fadff2322
giovannamoeller <giovannagmoeller@gmail.com>
2022-01-15 14:14:07 -0300

    introduction - variables and constants

---
+++ variables-constants.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var greeting = "Hello, playground"
+print(greeting)
+
+var name = "John"
+name = "Tim"
+
+let surname = "Doe"
+// surname = "McGraw
+
+// This is a comment
+/*
+ This is
+ a multi line comment
+*/
